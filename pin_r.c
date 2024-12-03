0e8ebc6e0c76c95046c2095a6c76e59345cc6962
AURORAMG07 <aurora727mg@gmail.com>
2024-12-03 13:27:13 +0100

    Add files via upload

---
+++ pin_r.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main (){
+int resposta;
+int errades;
+do{
+printf("Pin?: ");
+scanf("%d", &resposta);
+if(resposta!=1234) errades++;
+}while((resposta!=1234)&&(errades<3));
+if(errades==1234)
+     printf("Ok!\n");
+  else printf("Sistema bloquejat!\n");
+
+return;
+}
